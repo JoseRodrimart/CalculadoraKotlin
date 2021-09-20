a724620904563c49128ed28d9ab8479e107ed429
Jose Rodríguez <joserodriguez.alu@iespacomolla.es>
2021-09-20 21:58:13 +0200

    diseño creado

--- app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.calculadorakotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calculadora_basica)
+        val boton0: Button = findViewById(R.id.num0)
+        boton0.setOnClickListener { Log.w("hola","hola") }
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.num1 -> {/* do your code */}
+            else -> {Log.w("boton","1")}
+        }
+    }
+//    override fun onClick(v: View?) {
+////        val texto: TextView = findViewById<TextView>(R.id.resultado)
+//        Log.d("Boton Pulsado",v?.tag.toString())
+//        //texto.text = v?.tag.toString()
+//    }
 }
